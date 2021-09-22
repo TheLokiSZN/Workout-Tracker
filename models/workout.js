c1597efe2a3696df121969f3ddf53d9bbed24254
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-09-22 19:24:38 -0400

    finished index.js model

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Please enter workout"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Please enter workout"
+            },
+            duration: {
+                type: Number,
+                required: "Please enter a time"
+            },
+            weight: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+})
