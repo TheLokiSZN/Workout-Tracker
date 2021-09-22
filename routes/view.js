c4f4d8240a2bf5966ae1f2b0cc6c4389ab89abd7
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-09-22 19:36:12 -0400

    finished view.js

---
+++ routes/view.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const path = require("path");
+
+router.get('/exercise', (res, req) => {
+    res.sendFile(path.join(__dirname, '../public/exercise.html'))
+});
+
+router.get('/stats', (res, req) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'))
+});
+
+module.exports = router;
